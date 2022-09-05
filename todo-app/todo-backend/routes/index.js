e1dac241ed22a722cbfec498b429a908c561f661
Ashik Reji <ashikreji27@gmail.com>
2022-09-05 04:43:21 +0400

    Actually completed 12.5

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+const express = require('express');
+const router = express.Router();
+
+const configs = require('../util/config')
+
+let visits = 0
+
+/* GET index data. */
+router.get('/', async (req, res) => {
+  visits++
+
+  res.send({
+    ...configs,
+    visits
+  });
 });
 
 module.exports = router;
